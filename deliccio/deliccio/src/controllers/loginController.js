521394bda97a5d49f3ed0ac6afc50d341de2417a
cignacor <168667161+cignacor@users.noreply.github.com>
2025-04-08 22:03:02 -0500

    Delete deliccio directory

--- deliccio/deliccio/src/controllers/loginController.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-class LoginController{
-    getLoginPage =(req, res)=> {
-        res.sendFile(path.join(__dirname, '../public/index.html'))
-    }
-}
-module.exports=new LoginController();
